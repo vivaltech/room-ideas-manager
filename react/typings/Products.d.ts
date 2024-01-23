569b909fac7a53048ea551e1bc040cd1c4b443ec
Germán Bonacchi <german.bonacchi@valtech.com>
2024-01-23 15:32:56 -0300

    start implement new format

--- react/typings/Products.d.ts
+++ react/typings/Products.d.ts
@@ -5,16 +5,57 @@ export interface CsvProductData {
   productName: string
   brandId: string | number
   categoryIds: string | number | string[] | number[]
-  productSpecs: Array<{
-    name: string
-    values: string[]
-  }>
-  productAttributes: Array<{
-    name: string
-    value: string
-  }>
+  //productSpecs: Array<{
+  //  name: string
+  //  values: string[]
+  //}>
+
+  productSpecs_name_1: string
+  productSpecs_values_1: string[]
+  productSpecs_name_2: string
+  productSpecs_values_2: string[]
+  //productAttributes: Array<{
+  //  name: string
+  //  value: string
+  //}>
+  productAttributes_name_1: string
+  productAttributes_value_1: string
+  productAttributes_name_2: string
+  productAttributes_value_2: string
+  productAttributes_name_3: string
+  productAttributes_value_3: string
   productSlug: string
-  productImages: ImageData[]
+  //productImages: ImageData[]
+  productImages_id_1
+  productImages_url_1
+  productImages_alt_1
+  productImages_id_2
+  productImages_url_2
+  productImages_alt_2
+  productImages_id_3
+  productImages_url_3
+  productImages_alt_3
+  productImages_id_4
+  productImages_url_4
+  productImages_alt_4
+  productImages_id_5
+  productImages_url_5
+  productImages_alt_5
+  productImages_id_6
+  productImages_url_6
+  productImages_alt_6
+  productImages_id_7
+  productImages_url_7
+  productImages_alt_7
+  productImages_id_8
+  productImages_url_8
+  productImages_alt_8
+  productImages_id_9
+  productImages_url_9
+  productImages_alt_9
+  productImages_id_10
+  productImages_url_10
+  productImages_alt_10
   skuId?: string | number
   skuName: string
   skuExternalId?: string | number
@@ -22,11 +63,18 @@ export interface CsvProductData {
   skuManufacturerCode?: string | number
   skuIsActive: boolean
   skuWeight: number
-  skuDimensions: { width: number; height: number; length: number }
-  skuSpecs: Array<{
-    name: string
-    value: string
-  }>
+  //skuDimensions: { width: number; height: number; length: number }
+  skuWidth: number
+  skuHeight: number
+  skuLength: number
+  //skuSpecs: Array<{
+  //  name: string
+  //  value: string
+  //}>
+  skuSpecs_name_1: string
+  skuSpecs_value_1: string
+  skuSpecs_name_2: string
+  skuSpecs_value_2: string
   skuImages: string[]
   productTransportModal?: string | number
   productTaxCode?: string | number
